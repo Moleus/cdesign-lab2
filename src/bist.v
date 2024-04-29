b4ccffb30229134cb3260f1f3642271375808d85
Moleus <fafufuburr@gmail.com>
2024-04-29 14:37:33 +0300

    fix shifts and comparations

--- src/bist.v
+++ src/bist.v
@@ -132,7 +132,7 @@ always @(posedge clk) begin
             S0:
                 begin
                     dut_rst <= 0;
-                    if (flt_test != flt_test_prev && flt_test) begin
+                    if (~flt_test_prev && flt_test) begin
                         state <= S5;
                     end
                     if (~flt_default_mode_prev && flt_default_mode) begin
@@ -163,7 +163,6 @@ always @(posedge clk) begin
 
             S4:
                 begin
-                    out[15:12] <= 0;
                     out[15:0] <= dut_y;
                     state <= S0;
                 end
@@ -229,7 +228,7 @@ always @(posedge clk) begin
                     crc_bit <= dut_y[result_bit];
                     result_bit <= result_bit + 1;
 
-                    if (result_bit == 11) begin
+                    if (result_bit == 15) begin
                         state <= S13;
                     end
                 end
@@ -257,3 +256,4 @@ always @(posedge clk) begin
 end
 
 endmodule
+
--- src/bist_tb.v
+++ src/bist_tb.v
@@ -2,64 +2,46 @@
 
 module bist_tb;
 
-    reg clk = 1;
-    reg rst;
-    reg [7:0] a = 0;
-    reg [7:0] b = 0;
-    reg test = 0;
-    wire [15:0] out;
+  reg clk = 1;
+  reg rst;
+  reg [7:0] a = 0;
+  reg [7:0] b = 0;
+  reg test = 0;
+  wire [15:0] out;
 
-    bist bist(
-        .clk(clk),
-        .rst(rst),
-        .a(a),
-        .b(b),
-        .test(test),
-        .out(out)
-    );
+  bist bist (
+      .clk(clk),
+      .rst(rst),
+      .a(a),
+      .b(b),
+      .test(test),
+      .out(out)
+  );
 
-    always begin
-        #1  clk =  ! clk;
-    end
-    initial begin
-        rst = 1;
-        #20
-            rst = 0;
-        #20
+  always begin
+    #1 clk = !clk;
+  end
+  initial begin
+    rst = 1;
+    #20 rst = 0;
+    #20 a = 8'h2;
+    b = 8'h3;
+    #200000 $display("Output: %d^3 + sqrt(%d) = %d", a, b, a, out);
+    #20 test = 1;
+    #350 test = 0;
+    #250000 $display("Output: test %d, crc8 %b", out[15:8], out[7:0]);
+    #20 test = 1;
+    #350 test = 0;
+    #100 a = 8'h8e;
+    b = 8'hc2;
+    #1000 $display("Output: %d^3 + sqrt(%d) = %d", a, b, a, out);
+    #20 test = 1;
+    #350 test = 0;
+    #250000 $display("Output: test %d, crc8 %b", out[15:8], out[7:0]);
+    #20 test = 1;
+    #350 test = 0;
 
-            a = 8'h2;
-        b = 8'h3;
-        #200000
-            $display("Output: %d^3 + %d * %d = %d", a, b,a, out);
-        #20
-
-            test = 1;
-        #350
-            test = 0;
-        #250000
-            $display("Output: test %d, crc8 %b", out[15:8], out[7:0]);
-        #20
-            test = 1;
-        #350
-            test = 0;
-        #100
-
-            a = 8'h8e;
-        b = 8'hc2;
-        #1000
-            $display("Output: %d^3 + %d * %d = %d", a, b,a, out);
-        #20
-
-            test = 1;
-        #350
-            test = 0;
-        #250000
-            $display("Output: test %d, crc8 %b", out[15:8], out[7:0]);
-        #20
-            test = 1;
-        #350
-            test = 0;
-
-        $display("END");
-    end
+    $display("END");
+  end
 endmodule
+
